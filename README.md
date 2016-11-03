325585f3b91465bb2e1e06976ad53d467e66757c
Benjamin CARADEUC <bcaradeuc@rennes.sii.fr>
2016-11-03 16:46:35 +0100

    README + colors & validation & file writing + check platform

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # z-desktop
 
-_Work in progress..._
+_Create Desktop files in linux_
+
+## What is this ?
+
+This is a tool that allows you create desktop files useful for **linux**.
+
+I needed it when using elementaryOs (gnome based desktop environment) when installing some electron based applications & others.
+
+It will create files in `~/.local/share/applications` that will contain something like that : 
+
+```
+[Desktop Entry]
+Type=Application
+Encoding=UTF-8
+Name=Sample Application Name
+Comment=A sample application
+Exec=application
+Icon=application.png
+Terminal=false
+```
+After that this link will be available in the finder/explorer and you'll be able to add it in the dock. 
+
+## How To
+
+* `npm i -g z-desktop` or `npm i -g https://github.com/benavern/z-desktop` to install it (better installed generaly)
+* `zdkp` or `z-desktop` to start editing your desktop file
+
+## [License MIT](http://blog.caradeuc.info/MIT/#name=Benjamin%20Caradeuc&link=http://caradeuc.info)
--- index.js
+++ index.js
@@ -1,35 +1,72 @@
-const inquirer = require('inquirer');
-const clear = require('clear-screen');
-const ascii = require('figlet');
+#!/usr/bin/env node
+
+"use strict";
+
+// deps
+const inquirer =        require('inquirer');
+const clear =           require('clear-screen');
+const ascii =           require('figlet');
+const color =           require('chalk');
+const write =           require('output-file');
+const path =            require('path');
+const upperCamelCase =  require('uppercamelcase');
+const isValidPath =     require('is-valid-path');
+
+
+
+// Utils
+const HOME = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 
 let  questions = [
     {
         type: 'input',
         name: 'Name',
-        message: 'Application name :',
-        validate: input => !!input || "You must give your application a name.",
+        message: 'Application name' + color.yellow('*'),
+        validate: input => !!input || "You must give a name to your application.",
     },
     {
         type: 'input',
         name: 'Comment',
-        message: 'Application Description [optionnal] :',
+        message: 'Application Description',
     },
     {
         type: 'input',
         name: 'Exec',
-        message: 'Application\'s executable path (must be absolute) :',
-        validate: input => !!input && input.charAt(0) === '/' || "You must provide a valid file path to execute.",
+        message: 'Application\'s executable path (must be absolute)' + color.yellow('*'),
+        validate: input =>  {
+            if(!input) {
+                return "You must provide a file path to execute."
+            }
+            else if(input.charAt(0) === '/') {
+                return "You must provide an absolute file path to execute."
+            }
+            else if(isValidPath(input)) {
+                return "You must provide a valid file path to execute."
+            }
+            else return true;
+        },
     },
     {
         type: 'input',
         name: 'Icon',
-        message: 'Application\'s icon path (must be absolute) [optionnal] :',
-        validate: input => !input || input.charAt(0) === '/' || "You must provide a valid icon path.",
+        message: 'Application\'s icon path (must be absolute)',
+        validate: input => {
+            if(!input) {
+                return true; // it is optional
+            }
+            else if(input.charAt(0) === '/') {
+                return "You must provide an absolute icon path."
+            }
+            else if(isValidPath(input)) {
+                return "You must provide a valid icon path."
+            }
+            else return true;
+        },
     },
     {
         type: 'list',
         name: 'Terminal',
-        message: 'Show terminal ?',
+        message: 'Show terminal ?' + color.yellow('*'),
         choices: ['Yes', 'No'],
         default: 1,
         filter: answer => answer === 'Yes',
@@ -41,12 +78,67 @@ clear();
 
 // output some ascii art
 console.log(
-    ascii.textSync('Z-desktop', { horizontalLayout: 'full' })
+    color.cyan(
+        ascii.textSync('Z-desktop', { horizontalLayout: 'full' })
+    )
 );
+
+if(process.platform !== "linux") {
+    console.log(
+        color.bold.red('You must be on a linux platform to use this tool!')
+    )
+    process.exit(1)
+}
+
 
 // ask for information
 inquirer.prompt(questions).then(function (answers) {
 
     // make use of the information provided
-    console.log(answers);
+    // console.log(answers);
+
+    /** 
+     * Template
+     * 
+     * [Desktop Entry]
+     * Type=Application
+     * Encoding=UTF-8
+     * Name=Sample Application Name
+     * Comment=A sample application
+     * Exec=application
+     * Icon=application.png
+     * Terminal=false
+     */
+
+    // minimal data
+    let data = [
+        "[Desktop Entry]",
+        "Type=Application",
+        "Encoding=UTF-8",
+        "Name=" + answers.Name,
+        "Exec=" + answers.Exec,
+        "Terminal="+answers.Terminal,
+    ];
+
+    // optional data
+    if(!!answers.Comment) data.push("Comment="+answers.Comment)
+    if(!!answers.Icon) data.push("Icon="+answers.Icon)
+
+    const toWrite = data.join('\n');
+    const target =  path.join(HOME, ".local/share/applications", upperCamelCase(answers.Name) + ".desktop");
+
+    write(target, toWrite, function (err, dir) {
+        if(!err) {
+            console.log( "\n"
+                + color.green('The file has been created with success \n -> ') 
+                + color.bold.green(target + '\n') 
+                + "Its content is : \n\n"
+                + color.bold(toWrite)
+            );
+        }
+        else {
+            console.log( color.bold.red('An error occured : \n') + err );
+        }
+    });
+
 });
